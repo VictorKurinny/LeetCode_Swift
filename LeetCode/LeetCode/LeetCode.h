2ef794d8ea2c83c3cdd79af0d0e8fbfeec77aad5
Виктор Куринный <tyokman@gmail.com>
2020-02-27 21:38:18 +0200

    Add empty MacOS framework with unit tests.

---
+++ LeetCode/LeetCode/LeetCode.h
@@ -0,0 +1,19 @@
+//
+//  LeetCode.h
+//  LeetCode
+//
+//  Created by Victor Kurinny on 27.02.2020.
+//  Copyright © 2020 Victor Kurinny. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LeetCode.
+FOUNDATION_EXPORT double LeetCodeVersionNumber;
+
+//! Project version string for LeetCode.
+FOUNDATION_EXPORT const unsigned char LeetCodeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LeetCode/PublicHeader.h>
+
+
---
+++ LeetCode/LeetCodeTests/LeetCodeTests.swift
@@ -0,0 +1,14 @@
+//
+//  LeetCodeTests.swift
+//  LeetCodeTests
+//
+//  Created by Victor Kurinny on 27.02.2020.
+//  Copyright © 2020 Victor Kurinny. All rights reserved.
+//
+
+import XCTest
+@testable import LeetCode
+
+class LeetCodeTests: XCTestCase {
+
+}
